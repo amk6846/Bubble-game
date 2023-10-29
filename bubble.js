ba2799e088a6b3ac93e73531fac8e7970cb4ad5e
majid_khanDemo <amk6846@gmail.com>
2023-10-29 12:43:57 -0700

    initial commit

---
+++ bubble.js
@@ -0,0 +1,53 @@
+var timer = 60;
+var rn;
+var score = 0;
+function makebubble(){
+    var barbar = " ";
+for(i = 1; i<=175; i++)
+{
+    rn = Math.floor(Math.random()*10);
+    barbar += `<div class="bubble">${rn}</div>`;
+}
+document.querySelector(".panelbottom").innerHTML = barbar;
+}
+function TimerRun(){
+    var time = setInterval( function() {
+        if(timer>0)
+        {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+
+            clearInterval(time);
+            document.querySelector(".panelbottom").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);
+}
+function Newhit(){
+    rn = Math.floor(Math.random()*10);
+    document.querySelector("#newrand").textContent = rn;
+}
+function increasescore(){
+    score += 5;
+    document.querySelector("#scorec").textContent = score;
+}
+//query selector use to acces elements from html
+//number function use to convert string to int / target give me the div and text content give me the value
+//addeventlistner use to click and function give me the details
+     document.querySelector(".panelbottom")
+     .addEventListener("click",function(details){ 
+     var clickednum = Number(details.target.textContent); 
+     if(clickednum === rn)
+     {
+        increasescore();
+        makebubble();
+        Newhit();
+     }
+})
+TimerRun();
+makebubble();
+Newhit();
+
+
+
